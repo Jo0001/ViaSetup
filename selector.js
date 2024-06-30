45d11a7770c811a63098ecc7792b8b66546902a0
Jo0001 <47455182+Jo0001@users.noreply.github.com>
2024-06-30 17:18:44 +0200

    add selector

---
+++ selector.js
@@ -0,0 +1,139 @@
+const data = {
+    "type": "question",
+    "data": "Which type?",
+    "next": [
+        {
+            "type": "option",
+            "data": "Plugin",
+            "next": [
+                {
+                    "type": "answer",
+                    "data": "ViaVersion"
+                },
+                {
+                    "type": "answer",
+                    "data": "ViaBackwards"
+                },
+                {
+                    "type": "answer",
+                    "data": "ViaRewind"
+                }
+            ]
+        },
+        {
+            "type": "option",
+            "data": "Mod",
+            "next": [
+                {
+                    "type": "question",
+                    "data": "Client or Server?",
+                    "next": [
+                        {
+                            "type": "option",
+                            "data": "Server",
+                            "next": [
+                                {
+                                    "type": "answer",
+                                    "data": "ViaFabric"
+                                }
+                            ]
+                        },
+                        {
+                            "type": "option",
+                            "data": "Client",
+                            "next": [
+                                {
+                                    "type": "answer",
+                                    "data": "ViaFabric"
+                                },
+                                {
+                                    "type": "answer",
+                                    "data": "ViaFabricPlus"
+                                },
+                                {
+                                    "type": "answer",
+                                    "data": "ViaForge"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "type": "option",
+            "data": "Platform/App",
+            "next": [
+                {
+                    "type": "answer",
+                    "data": "ViaAAS"
+                }, {
+                    "type": "answer",
+                    "data": "ViaProxy"
+                }
+            ]
+        }
+    ]
+};
+
+let lasttype = null;
+let content = document.getElementById("content");
+let resetbtn = document.getElementById("resetbtn");
+
+function create(obj) {
+
+    let type = obj.type;
+    let data = obj.data;
+    let next = obj.next;
+
+    if (type !== "option" && lasttype !== "answer") {
+        content.innerHTML = "";
+    }
+
+    let q = createElement(type);
+    q.innerText = data;
+    content.appendChild(q);
+
+
+    if (type === "question" || type === "option") {
+        q.addEventListener('click', function () {
+            resetbtn.style.display = "block";
+            next.forEach((i) => {
+                create(i);
+            });
+        });
+    }
+    lasttype = type;
+
+    if (type === "question") {
+        next.forEach((i) => {
+            create(i);
+        });
+    }
+}
+
+function createElement(type) {
+    if (type === "option") {
+        const b = document.createElement("button");
+        b.classList = "btn btn-outline-primary selector";
+        return b;
+    } else {
+        const p = document.createElement("p");
+        p.classList = type;
+        return p;
+    }
+}
+
+
+function start() {
+    document.getElementById("help").style.display = "none";
+    create(data);
+}
+
+function reset() {
+    content.innerHTML = "";
+    resetbtn.style.display = "none";
+    start();
+}
+
+//start();
